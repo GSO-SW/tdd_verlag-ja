bf39a8038abfed88c80f1cd63613901701a8a594
JalalHamo <jalal.h2@gso.schule.koeln>
2023-01-30 02:12:48 +0100

    UserStory2 Tests erstellt

---
+++ VerlagTests/ISBNTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Verlag;
+
+namespace VerlagTests
+{
+    [TestClass]
+    public class ISBNTests
+    {
+        [TestMethod]
+        public void Buch_ISBNKannGespeichertWerden()
+        {
+            //Arrange
+             long iSBN13 = 326312564798;
+
+            //Act 
+            Buch b = new Buch("Nadia", "Ich bin eure Stimme.");
+            b.ISBN13 = iSBN13;
+
+            //Assert
+            Assert.AreEqual(iSBN13, b.ISBN13);
+        }
+
+        [TestMethod]
+        public void ISBN13_OhnePruefzifferWirdAutoVergeben()
+        {
+            //Arrange
+            long iSBNOhnePZ = 978377043614;
+            long iSBNErwartet = 9783770436149;
+
+            //Act
+            Buch b = new Buch("Nadia", "Ich bin eure Stimme.");
+            b.ISBN13 = iSBNOhnePZ;
+
+            //Assert
+            Assert.AreEqual(iSBNErwartet, b.ISBN13);
+        }
+
+        [TestMethod]
+        public void ISBN10_KannAusISBN13ErechnetWerden()
+        {
+            //Arrange
+            long iSBN13 = 9783770436064;
+            long iSBN10 = 3770436067;
+
+            //Act
+            Buch b = new Buch("Nadia", "Ich bin eure Stimme.");
+            b.ISBN13 = iSBN13;
+
+            //Assert
+            Assert.AreEqual(iSBN10, b.ISBN10);
+        }
+    }
+}
